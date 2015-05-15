f0a14758c808185bfc9d8b47b19f9d2cae8b75a3
Hugo1991 <hugofernandezvisier@gmail.com>
2015-05-15 11:35:04 +0200

    clases creadas

---
+++ src/main/java/blog/Usuario.java
@@ -0,0 +1,56 @@
+package blog;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idUsuario;
+	
+	private long DNI;
+	private String tarjetaSocio;
+	private String nombre;
+	private String apellidos;
+	private Date fechaNac;
+	private String email;
+	
+	public Usuario(){}
+	public Usuario(long dni,String tarjetaSocio,String nombre,String apellidos,Date fecha,String email){
+		setDNI(dni);
+		setTarjetaSocio(tarjetaSocio);
+		setNombre(nombre);
+		setApellidos(apellidos);
+		setFechaNac(fecha);
+		setEmail(email);
+	}
+	public void setDNI(long dni){}
+	public void setTarjetaSocio(String tarjetaSocio){}
+	public void setNombre(String nombre){}
+	public void setApellidos(String apellidos){}
+	public void setFechaNac(Date fecha){}
+	public void setEmail(String email){}
+	public Long getDNI(){
+		return DNI;
+	}
+	public String getTarjetaSocio(){
+		return tarjetaSocio;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public String getApellidos(){
+		return apellidos;
+	}
+	public Date getFechaNac(){
+		return fechaNac;
+	}
+	public void pedirCita(Medico medico,Date fecha){
+		
+	}
+}
