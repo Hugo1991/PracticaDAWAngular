7693b04edc1bb38d76444d9d04f92c39e83bc523
cruzmaster <sefi-sefi@hotmail.com>
2015-05-15 13:16:56 +0200

    Interfaz gráfica

--- src/main/resources/static/app/routeconfig.js
+++ src/main/resources/static/app/routeconfig.js
@@ -4,7 +4,13 @@ RouteConfig.$inject = [ '$routeProvider' ];
 
 function RouteConfig($routeProvider) {
 
-	$routeProvider.when('/', { templateUrl : "templates/postlist.html" });
+	$routeProvider.when('/', { templateUrl : "templates/principal.html" });
+	$routeProvider.when('/home', { templateUrl : "templates/principal.html" });
+	$routeProvider.when('/desc', { templateUrl : "templates/description.html" });
+	$routeProvider.when('/cita', { templateUrl : "templates/cita.html/" });
+	$routeProvider.when('/showcase', { templateUrl : "templates/showcase.html/" });
+	$routeProvider.when('/frec', { templateUrl : "templates/frecuentes.html/" });
+	$routeProvider.when('/registro', { templateUrl : "templates/registro.html/" });
 	$routeProvider.when('/newpost', { templateUrl : "templates/newpost.html" });
 	$routeProvider.when('/post/:id', { templateUrl : "templates/post.html" });
 
